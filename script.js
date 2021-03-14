5bb402078fa2a8b886ba8fd13ae7c9c861321358
C0792103 <79386160+C0792103@users.noreply.github.com>
2021-03-13 21:03:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const rows=document.querySelectorAll('.rows');
+const cost=document.querySelector('.cost');
+
+let totalSeats=document.querySelector('.totalSeats');
+
+let movie=document.getElementById('movie');
+
+const seats=documentdocument.querySelectorAll('.seats:not(.occupied');
+
+let removeSeats=false;
+let seatRow=0;
+let seatNumber=0;
+
+
+
+if (Element.classList.value == 'seats') {
+    Element.classList.add('selectedSeats');
+    
+    RemoveSeat = false;
+} else {
+    Element.classList.remove('selectedSeats');
+
+    removeSeat=true;
+}
